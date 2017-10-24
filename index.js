fa9ca5ff1715a4772b36dd0273f7ed32b225c912
Kyle Nielsen <nielsen.kyle93@utexas.edu>
2017-10-24 15:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   if string === string.toUpperCase{
     return "YES INDEED!"
   }
-  if string === string.toLowercase{
+  if string === string.toLowerCase{
     return "I can't hear you"
   }
   else {
