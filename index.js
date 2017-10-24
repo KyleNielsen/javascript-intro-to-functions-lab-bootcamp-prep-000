3b7a06b1d92aa554c8c3375a1f0e7ac709f27b19
Kyle Nielsen <nielsen.kyle93@utexas.edu>
2017-10-24 16:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
-  var love = "I love you, Grandma"
+  var love = "I love you, Grandma."
   if (string === love) {
     return "I love you, too."
   }
